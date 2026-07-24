4c1a3e48a237804e045ed7a0946510eeddb65a7e
Abinaya1226 <abi.pkumaran@gmail.com>
2026-07-24 22:16:13 +0530

    Create DemoApplication.java

---
+++ Week2/Spring Data JPA with Spring Boot,Hibernate/differences/src/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class,args);
+
+        System.out.println("JPA, Hibernate and Spring Data JPA Example");
+
+    }
+
+}
