e420e5f1dcf0bd9aa7fb9186070ae400612420f5
Abinaya1226 <abi.pkumaran@gmail.com>
2026-07-29 10:49:45 +0530

    Create home.ts

---
+++ Week5/Angular/Hands_ON_3/home.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+
+  searchTerm = '';
+
+  availableCourses = 12;
+
+  ngOnInit(): void {
+    console.log('HomeComponent initialised — courses loaded');
+  }
+
+  ngOnDestroy(): void {
+    console.log('HomeComponent destroyed');
+  }
+
+  onEnrollClick(): void {
+    this.message = 'Enrollment opened!';
+  }
+}
